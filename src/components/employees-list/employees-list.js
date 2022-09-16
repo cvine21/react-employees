2c81395ff4016d824cd67f2a4079a67242d83cd7
Claudine Vine <rafarakh@gmail.com>
2022-09-16 18:14:09 +0300

    init

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,15 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+
+import "./employees-list.css";
+
+const EmployeesList = ({ data }) => {
+	return (
+		<ul className="app-list list-group">
+			{data.map((item) => (
+				<EmployeesListItem {...item} />
+			))}
+		</ul>
+	);
+};
+
+export default EmployeesList;
